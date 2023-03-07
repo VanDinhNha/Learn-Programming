e085a1d79d0ddebd6eeda9dfd915c659ed56f1e9
VanDinhNha <VanDinhNha20010404@gmail.com>
2023-03-07 19:10:38 +0700

    active menu

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-const activeMenu = (opj) => {
+const checkMenu = document.querySelector("#menu_checkbox");
+const menu = document.querySelector(".menu");
+const mobile_menu_background = document.querySelector(".mobile-menu-background");
+const icon_menu = document.querySelector(".icon-menu");
+
+const selectMenu = (opj) => {
     if(opj.childNodes.length > 3)
-        opj.childNodes[3].classList.toggle("menu-item__child")
+        opj.childNodes[3].classList.toggle("menu-item__child");
 };
+
+checkMenu.onclick = (e) => {
+    menu.classList.toggle("toggle-menu");
+    mobile_menu_background.hasAttribute("style") ?
+    mobile_menu_background.removeAttribute("style") :
+    mobile_menu_background.setAttribute("style", "display: block")
+};
+
+function scrolledMenu(){
+    if(!menu.classList.contains("toggle-menu")){
+        menu.scrollTop > 30 ? 
+        icon_menu.setAttribute("style", `margin-top: -${menu.scrollTop}px`) : 
+        icon_menu.removeAttribute("style")
+    }
+}
+
+menu.addEventListener('scroll', scrolledMenu);
+
+function scrolledWindow(event){
+    if(menu.classList.contains("toggle-menu")){
+        window.pageYOffset > 30 ? 
+        icon_menu.setAttribute("style", `margin-top: -${window.pageYOffset}px`) : 
+        icon_menu.removeAttribute("style")
+    }
+}
+
+window.addEventListener('scroll', scrolledWindow);
