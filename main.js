184055ca5c6f1376310ab54ad49613f882602658
VanDinhNha <VanDinhNha20010404@gmail.com>
2023-04-03 10:07:30 +0700

    catch error api

--- main.js
+++ main.js
@@ -11,13 +11,18 @@ const endpointMenu = "http://learn-programming-test.com/api/MENU";
 const endpointContent = "http://learn-programming-test.com/api/CONTENT/";
 // const endpoint = "./data.json"
 
-const showMenuChild = (obj) => {
-    console.log(obj.childNodes[1].childNodes);
-    if(obj.childNodes.length > 3){
-        obj.childNodes[1].childNodes[1].classList.toggle("acctive__menu-item__parent--line")
-        obj.childNodes[3].classList.toggle("menu-item__child");
-        obj.childNodes[1].classList.toggle("acctive__menu-item__parent")
-        obj.childNodes[1].childNodes[7].classList.toggle("acctive__hover-test")
+const showMenuChild = (obj, obj_id = null) => {
+    if(obj !== null){
+        if(obj.childNodes.length > 3){
+            obj.childNodes[1].childNodes[1].classList.toggle("acctive__menu-item__parent--line")
+            obj.childNodes[3].classList.toggle("menu-item__child");
+            obj.childNodes[1].classList.toggle("acctive__menu-item__parent")
+            obj.childNodes[1].childNodes[7].classList.toggle("acctive__hover-test")
+            if(obj_id !== null){
+                const menuChild = document.getElementById(obj_id["id-menu"]+"_"+obj_id["id-content"]);
+                menuChild.classList.add("acctive__menu-item__link");
+            }
+        }
     }
 };
 
@@ -54,18 +59,18 @@ const copy_code = (opj) => {
     navigator.clipboard.writeText(opj.parentElement.childNodes[3].childNodes[1].textContent)
 }
 
-function showMenuData(icon = "", name = "", menu_child){
+function showMenuData(id = "", icon = "", name = "", menu_child){
     let template_child = "";
     if(menu_child !== null){
         if(menu_child.length > 0 && Array.isArray(menu_child)){
             menu_child.forEach(item =>{
-                template_child += `<a href="/?id=${item.ID_CONTENT}" class="menu-item__link">
+                template_child += `<a href="/?id-menu=${id}&id-content=${item.ID_CONTENT}" id="${id}_${item.ID_CONTENT}" class="menu-item__link">
                     ${item.NAME}
                 </a>`;
             })
         }
     }
-    const template = `<li class="menu-item" onclick="showMenuChild(this)">
+    const template = `<li class="menu-item" id="${id}" onclick="showMenuChild(this)">
     <div class="menu-item__parent">
         <span class="menu-item__parent--line"></span>
         ${icon}
@@ -83,13 +88,14 @@ async function loadDataMenu(){
     const response = await fetch(endpointMenu);
     const menu_item = await response.json();
     menu_item.forEach(item => {
-        showMenuData(item.ICON, item.NAME, item.MENU_CHILD);
+        showMenuData(item.ID, item.ICON, item.NAME, item.MENU_CHILD);
     });
+    getUrlVars(url)
 }
 
-loadDataMenu();
+loadDataMenu().catch(handleError);
 //&lt;
-function showDataContent(value){
+function showDataContent(value, obj_id){
     let template = `<div class="content-body-title">
             <span class="content-body-title--text">
                 ${value.TITLE}
@@ -97,47 +103,46 @@ function showDataContent(value){
         </div>
         <p class="content-body--describe">${value.DESCRIBE}</p>
         `
-        // if(value.attention.length > 0){
-        //     template += `<P class="content-body--attention">Lưu ý: ${value.attention}</P>`
-        // }
-        if(value.CODE.length > 0){
-            value.CODE.forEach(itemDetail => {
-                template += `<div class="content-body-detail">
-            <label class="content-body-detail__title--text">${itemDetail.TITLE}</label>
-            <div class="content-body-detail__code">
-                <button class="btn-copy" onclick="copy_code(this)">
-                    <svg width="20px" height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#ffffff" d="M768 832a128 128 0 0 1-128 128H192A128 128 0 0 1 64 832V384a128 128 0 0 1 128-128v64a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64h64z"></path><path fill="#ffffff" d="M384 128a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64V192a64 64 0 0 0-64-64H384zm0-64h448a128 128 0 0 1 128 128v448a128 128 0 0 1-128 128H384a128 128 0 0 1-128-128V192A128 128 0 0 1 384 64z"></path></g></svg>
-                </button>
-                <deckgo-highlight-code language="${itemDetail.LANGUAGE}">
-                    <code slot="code">
-${itemDetail.CODE}
-                    </code>
-                </deckgo-highlight-code>`
-                if(itemDetail.DESCRIBE !== null){
-                    template += `<p class="content-body-detail__code--describe">${itemDetail.DESCRIBE}</p>`
-                }
-                if(itemDetail.NOTE !== null){
-                    template += `<P class="content-body-detail__code--attention">Lưu ý: ${itemDetail.NOTE}</P>`
-                }
+    if(value.NOTE !== null){
+        template += `<P class="content-body--attention">Lưu ý: ${value.NOTE}</P>`
+    }
+    if(value.CODE.length > 0){
+        value.CODE.forEach(itemDetail => {
+            template += `<div class="content-body-detail">
+        <label class="content-body-detail__title--text">${itemDetail.TITLE}</label>
+        <div class="content-body-detail__code">
+            <button class="btn-copy" onclick="copy_code(this)">
+                <svg width="20px" height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#ffffff" d="M768 832a128 128 0 0 1-128 128H192A128 128 0 0 1 64 832V384a128 128 0 0 1 128-128v64a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64h64z"></path><path fill="#ffffff" d="M384 128a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64V192a64 64 0 0 0-64-64H384zm0-64h448a128 128 0 0 1 128 128v448a128 128 0 0 1-128 128H384a128 128 0 0 1-128-128V192A128 128 0 0 1 384 64z"></path></g></svg>
+            </button>
+            <deckgo-highlight-code language="${itemDetail.LANGUAGE}">
+            <code slot="code">${itemDetail.CODE}</code>
+            </deckgo-highlight-code>`
+            if(itemDetail.DESCRIBE !== null){
+                template += `<p class="content-body-detail__code--describe">${itemDetail.DESCRIBE}</p>`
+            }
+            if(itemDetail.NOTE !== null){
+                template += `<P class="content-body-detail__code--attention">Lưu ý: ${itemDetail.NOTE}</P>`
+            }
+            
+            // <div class="cotent-body-detail__code--demo">
                 
-                // <div class="cotent-body-detail__code--demo">
-                    
-                // </div>
-           template += ` </div>
-        </div>`
-            })
-        }
-        console.log(template)
-        content_body.innerHTML = template
+            // </div>
+        template += ` </div>
+    </div>`
+        })
+    }
+    content_body.innerHTML = template
+    const acctiveMenu = document.getElementById(obj_id["id-menu"]);
+    showMenuChild(acctiveMenu, obj_id);
 }
 
 async function loadDataContent(id){
     if(Object.keys(id).length > 0){
-        const response = await fetch(endpointContent + id.id);
+        const response = await fetch(endpointContent + id["id-content"]);
         const content_item = await response.json();
-        console.log(content_item);
-        showDataContent(content_item);
+        showDataContent(content_item, id);
     }
+    Loading.removeAttribute("style");
 }
 
 function getUrlVars(url) {
@@ -146,8 +151,10 @@ function getUrlVars(url) {
         const parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
             vars[key] = value;
         });
-        loadDataContent(vars);
+        loadDataContent(vars).catch(handleError);;
     }
 }
 
-getUrlVars(url)
+function handleError(err){
+    window.location="/error.html";
+}
